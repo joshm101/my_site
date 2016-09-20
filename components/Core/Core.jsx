b1f37836d488cb2d8cc8f6b084ebaec5f39f4f5f
Joshua Mora <jshm101@gmail.com>
2016-09-20 00:06:04 -0700

    Fully working WorkItem component to represent some work (website) I've done

--- components/Core/Core.jsx
+++ components/Core/Core.jsx
@@ -14,32 +14,51 @@ class Core extends React.Component {
   render() {
     var workStyle = {
       marginLeft: "auto",
-      marginRight: "auto"
-    }
+      marginRight: "auto",
+      marginTop: "20px"
+    };
     
-    var workArr = [{title: "Blackwashed.com",
-                    description: "Portfolio for photo albums and videos. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    var workArr = [{
+                    title: "Blackwashed.com",
+                    description: "Portfolio for photo albums and videos. Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
                     images: [
-                              {img_src: "../../assets/img/n8iaOQw.jpg",
+                              {img_src: "../../assets/img/blackwashedcom.PNG",
                                key: "0",
                                current: 'true'},
-                              {img_src: "../../assets/img/town.jpg",
+                              {img_src: "../../assets/img/blackwashedcom2.PNG",
                                key: "1",
                                current: 'false'},
-                              {img_src: "../../assets/img/gJB0DJs.jpg",
+                              {img_src: "../../assets/img/blackwashedcom3.PNG",
                                key: "2",
-                               current: 'false'},
-                              {img_src: "../../assets/img/rvKm09g.jpg",
-                               key: "3",
                                current: 'false'}
                               ],
                       workUrl: "http://blackwashed.com"
-                    }];
+                    },
+                    {
+                      title: "somesite.com",
+                      description: "short description of site that I made lorem ipsumshort description of site that I made lorem ipsum",
+                      images: [
+                        {img_src: "../../assets/img/1n1tiMo.png",
+                         key: "0",
+                         current: 'true'},
+                        {img_src: "../../assets/img/2zJPaOJ.png",
+                         key: '1',
+                         current: 'false'}
+                      ],
+                      workUrl: "https://google.com"
+                    }
+                  ];
 
-    return (<MuiThemeProvider style={workStyle}>
+    return (<MuiThemeProvider>
+              <div>
               <div style={workStyle}>
                 <WorkItem work={workArr[0]}>
                 </WorkItem>
+              </div>
+              <div style={workStyle}>
+                <WorkItem work={workArr[1]}>
+                </WorkItem>
+              </div>
               </div>
             </MuiThemeProvider>);
   }
--- components/WorkItem/WorkItem.jsx
+++ components/WorkItem/WorkItem.jsx
@@ -12,6 +12,7 @@ export default class WorkItem extends React.Component {
     super(props);
     this.state = {
       expanded: false,
+      longDesc: false
     };
   }
   
@@ -37,7 +38,8 @@ export default class WorkItem extends React.Component {
       width: "256px",
       position: "relative",
       marginLeft: "auto",
-      marginRight: "auto"
+      marginRight: "auto",
+      minHeight: "350px"
     };
     
     var fabStyle = {
@@ -51,11 +53,56 @@ export default class WorkItem extends React.Component {
       marginLeft: "25%",
       marginRight: "25%"
     };
+    
+    var cardTextStyle = {
+      padding: "10px"
+    }
+    
+    var shortDescription = "";
+    
+    var shortDescTh = 95;
+    
+    // this piece of code creates a short description with a trailing ellipsis
+    // if the length of the description is more than 95 characters
+    // 95 is chosen because that is around the end of the third line
+    // in our workitem card, so there is a good chance we will need
+    // a fourth line in our description, justifying "show more" even
+    // if it's only a couple more words.
+    if (this.props.work.description.length > shortDescTh) {
+      
+      // length of description more than 95 characters
+      var desc = this.props.work.description;
+      
+      // get the threshold index in the string
+      var ind = shortDescTh;
+      
+      // move to the end of current word at threshold index.
+      for (var i = ind; desc[i] !== " " && desc[i]; ++i) {++ind;}
+        
+      if (desc[ind] === " ") {
+        // we reached the end of a word and there is at least
+        // one more word in the description.
+        shortDescription = desc.slice(0, ind);
+        shortDescription = shortDescription + "...";
+        console.log("shortDescription: ", shortDescription);
+        this.state.longDesc = true;
+      } else {
+        // if we do not have a space at the end of the last word
+        // in our short description, then that means we have
+        // reached the end of the description. No need for
+        // a show more button. The state is updated to reflect that
+        // for later use.
+        this.state.longDesc = false;
+      } 
+    }
     return(
       <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange} style={cardStyle}>
         <CardMedia>
           <ImageSlideshow images={this.props.work.images} />
         </CardMedia>
+      
+        // this floating action button will 
+        // take the user to the displayed website.
         <FloatingActionButton style={fabStyle}
                               zDepth={3}
                               mini={true}
@@ -64,22 +111,51 @@ export default class WorkItem extends React.Component {
           <NavigationArrowForward />
         </FloatingActionButton>
         <CardTitle title={this.props.work.title} />
-        <CardText expandable={true}>                              {this.props.work.description}
-        </CardText>
+          {this.state.longDesc === true ?
+            // we have a long description so make use
+            // of short description and full description
+            
+            this.state.expanded ? 
+              // if we are in the card's expanded state
+              // then display the full description
+              <CardText expandable={true} style={cardTextStyle}>
+              {this.props.work.description}
+              </CardText>
+
+            : 
+              // if we are NOT in the expanded state, then
+              // display the short description
+              <CardText style={cardTextStyle}>{shortDescription}</CardText>
+
+          : 
+            // if the description is shorter than our threshold, display as is.
+            <CardText style={cardTextStyle}>{this.props.work.description}</CardText>
+
+          }
         <CardActions>
-      
-        // Seems state starts as true even though the
-        // constructor sets expanded to false. Utilizing
-        // negation offer fixes for now. Need to figure out
-        // why this.state.expanded is not properly set by
-        // constructor.
-        {!this.state.expanded ? 
-            <FlatButton label="Show more" onTouchTap={this.handleExpand.bind(this)}
-            style={showStyle} /> 
+          // Seems state starts as true even though the
+          // constructor sets expanded to false. Utilizing
+          // negation offer fixes for now. Need to figure out
+          // why this.state.expanded is not properly set by
+          // constructor.
+          
+          {this.state.longDesc === true ? 
+            // if we have a description longer than threshold limit
+            !this.state.expanded ? 
+              // if the card is not expanded, show the "SHOW MORE" button which
+              // expands the card when clicked.
+              <FlatButton label="Show more" onTouchTap={this.handleExpand.bind(this)}
+              style={showStyle} /> 
+            :
+              // if the card is expanded, show the "SHOW LESS" button which
+              // shrinks the card when clicked.
+              <FlatButton onTouchTap={this.handleReduce.bind(this)} label="Show less"
+              style={showStyle} />
+
           :
-            <FlatButton onTouchTap={this.handleReduce.bind(this)} label="Show less"
-            style={showStyle} />
-        }
+            // if the description is shorter than our threshold, no buttons.
+            <div></div>
+          }
         </CardActions>
       </Card>
     );
